70c11151004c0e660269a39defe32e7d58483796
Kaushlendra <koshlendratomar31@gmail.com>
2020-08-21 16:12:08 +0530

    css fixes

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Course do
   permit_params :title, :content, :start_datetime, :end_datetime, :length_of_students
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :content
+      f.input :start_datetime,  as: :date_time_picker
+      f.input :end_datetime,  as: :date_time_picker
+      f.input :length_of_students
+    end
+    f.actions
+  end
 end
